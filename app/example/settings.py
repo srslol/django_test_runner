404da0325caddb6f688e6c26b743b8ec828813df
Scott Myers <scott.t.myers@gmail.com>
2024-07-06 16:21:03 -0600

    Update settings.py
    adding ports

--- app/example/settings.py
+++ app/example/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = env('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
 
-# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ("localhost", "127.0.0.1", "0.0.0.0", '192.168.0.40'))
-ALLOWED_HOSTS = ['0.0.0.0','192.168.0.0/24']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ("localhost", "127.0.0.1", "0.0.0.0", "192.168.0.200", "192.168.0.40", "192.168.0.0/24"))
+#ALLOWED_HOSTS = ['0.0.0.0','192.168.0.0/24']
 
 
 # Application definition
